71f6340e51501b038f3c22478415275e3c3ec492
KasunDimantha <kasundimantha97@gmail.com>
2024-03-07 10:50:54 +0530

    create express routers

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const workoutRoutes = require('./routes/Workouts')
 
 
 // express app
 const app = express()
 
+//midlware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 // routes
-app.get('/', (req, res) => {
-    res.json({message: 'Hello World!'})
-})
+app.use('/api/workouts' , workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => console.log('Server is listening on port ', process.env.PORT))
