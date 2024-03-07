d1806ba191954b229255c0a5823040eba94e9573
KasunDimantha <kasundimantha97@gmail.com>
2024-03-07 14:21:59 +0530

    complete controller workout and create fontend react app

--- backend/routes/Workouts.js
+++ backend/routes/Workouts.js
@@ -1,41 +1,29 @@
 const express = require('express');
-const Workout = require('../models/WorkoutModel');
+const {
+    getWorkout,
+    getWorkouts,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout,
+} = require('../controllers/workoutController');
+
 
 const router = express.Router();
 
 
 //get all workout
-router.get('/', (req, res) => {
-    res.json({ message: 'Get All Workout' });
-})
+router.get('/', getWorkouts)
 
 // get a single workout
-router.get('/:id', (req, res) => {
-    res.json({ message: 'Get a single workout' });
-})
+router.get('/:id', getWorkout)
 
 // post a new workout
-router.post('/', async (req, res) => {
-    const { title, reps, load} = req.body;
-
-    try {
-        const workout = await Workout.create({ title, reps, load });
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-
-    res.json({ message: 'Post a new workout' });
-})
+router.post('/', createWorkout)
 
 // Delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({ message: 'Delete a workout' });
-})
+router.delete('/:id', deleteWorkout)
 
 // Update a workout
-router.patch('/:id', (req, res) => {
-  res.json({ message: 'Update a workout' });
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router;
