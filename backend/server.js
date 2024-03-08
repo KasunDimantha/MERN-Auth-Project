6017da0443ffe039b20e27e86c7eac8db79e08cd
KasunDimantha <kasundimantha97@gmail.com>
2024-03-08 16:24:02 +0530

    create signup hash method

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/Workouts')
+const userRoutes = require('./routes/user')
 
 
 // express app
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts' , workoutRoutes)
+app.use('/api/user' , userRoutes)
 
 
 // connect to db
