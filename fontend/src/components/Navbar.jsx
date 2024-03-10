52d395b418ceef5c9cee28c0fff3b00c12c21f96
KasunDimantha <kasundimantha97@gmail.com>
2024-03-10 08:33:45 +0530

    create login signup page

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 function Navbar() {
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout()
+  }
   return (
     <div>
       <header>
         <div className="container">
             <Link to='/'><h1>Workout Buddy</h1></Link>
             <nav>
+              <div>
+                <button onClick={handleClick}>Log Out</button>
+              </div>
               <div>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Signup</Link>
